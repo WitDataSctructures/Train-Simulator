05f6d85a6615b3b88fcb3e1299eed6069420ece5
Ford Polia <poliaf@wit.edu>
2016-02-17 09:39:08 -0500

    Updated passenger class

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,6 +1,21 @@
 
 public class Passenger {
-	public enum Direction{
+	public static enum Direction{
 		INBOUND, OUTBOUND
+	}
+	
+	private Direction passengerDirection;
+	private Station destination;
+	
+	public Passenger(Station dest, Direction dir){
+		destination = dest;
+		passengerDirection = dir;
+	}
+	
+	public Direction getDirection(){
+		return passengerDirection;
+	}
+	public Station getDestination(){
+		return destination;
 	}
 }
