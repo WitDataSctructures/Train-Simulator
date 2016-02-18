40f334aba1274cc944dfd213b4a18be80e1f319c
Ford Polia <poliaf@wit.edu>
2016-02-18 09:14:34 -0500

    Removed direction from passenger

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,16 +1,11 @@
 
 public class Passenger {
-	private Train.Direction passengerDirection;
 	private TrainRoute.Station destination;
 	
 	public Passenger(TrainRoute.Station dest, Train.Direction dir){
 		destination = dest;
-		passengerDirection = dir;
 	}
 	
-	public Train.Direction getDirection(){
-		return passengerDirection;
-	}
 	public TrainRoute.Station getDestination(){
 		return destination;
 	}
