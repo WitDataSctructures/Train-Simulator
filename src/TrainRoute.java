6e985dd05c359d9bda0c4a58e86fab95d2000c27
Jake Mathews <mathewsj2@wit.edu>
2016-02-18 09:04:36 -0500

    Added ability to check distance to previous station and get the enum of the previous station

--- src/TrainRoute.java
+++ src/TrainRoute.java
@@ -3,19 +3,26 @@ public class TrainRoute {
 
 	public static void main(String[] args) {
 		new TrainRoute();
-		System.out.println(Station.StationF.getNextStation());
+		// System.out.println(Station.StationF.getNextStation());
+		// System.out.println(Station.StationF.getPreviousStation());
+		// System.out.println(Station.StationA.getNextStation());
+		// System.out.println(Station.StationA.getPreviousStation());
+		// System.out.println(Station.StationD.getNextStation());
+		// System.out.println(Station.StationD.getPreviousStation());
 	}
 
 	public TrainRoute() {
 		// Update loop
-		while (true) {
+		boolean update = true;
+		int numberOfUpdates = 10;
+		while (update && numberOfUpdates > 0) {
 			// Update stations
 			for (Station station : Station.values()) {
 				station.getStation().update();
 			}
 
 			// Update train
-
+			numberOfUpdates--;
 		}
 	}
 
@@ -23,6 +30,7 @@ public class TrainRoute {
 		StationA(0, 1), StationB(1, 2), StationC(2, 3), StationD(3, 4), StationE(4, 3), StationF(5, 2);
 
 		private final static Station headStation = StationA;
+		private final static Station tailStation = StationF;
 
 		private final int value;
 		private final int distance;
@@ -42,6 +50,10 @@ public class TrainRoute {
 			return distance;
 		}
 
+		public int getDistanceToPrevious() {
+			return getPreviousStation().getDistanceToNext();
+		}
+
 		public TrainStation getStation() {
 			return station;
 		}
@@ -53,6 +65,15 @@ public class TrainRoute {
 				}
 			}
 			return headStation;
+		}
+
+		public Station getPreviousStation() {
+			for (Station s : Station.values()) {
+				if (s.value == value - 1) {
+					return s;
+				}
+			}
+			return tailStation;
 		}
 	};
 }
